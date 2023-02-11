f48625ee0530dbc7d207e5ca414fff6767c91fe4
Rishi P <rishipatel1997@gmail.com>
2023-02-11 10:58:16 -0500

    Added some setup for sprite animation

---
+++ FarmGame/Assets/Animator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public Sprite newSprite;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // do nothing here??
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //change the sprite here to the second sheep
+    }
+}
