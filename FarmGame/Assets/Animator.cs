b7a93ac4430ba61ca4d5d6ba14115b3a3c4b976a
Rishi P <rishipatel1997@gmail.com>
2023-02-12 12:59:09 -0500

    Player is now animated when you hold WASD

--- FarmGame/Assets/Animator.cs
+++ FarmGame/Assets/Animator.cs
@@ -5,17 +5,104 @@ using UnityEngine;
 public class Animator : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public Sprite newSprite;
- 
+    public Sprite[] spriteArray;
+    private float timer = 0;
+    private int spriteFrame = 0;
+    public float animationSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        // do nothing here??
+        // set sprite to first frame
+        spriteRenderer.sprite = spriteArray[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        //change the sprite here to the second sheep
+        // if a key is being held down we play an animation
+        if (Input.GetKey(KeyCode.W))
+        {
+            playUpAnimation();
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            playLeftAnimation();
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playDownAnimation();
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playRightAnimation();
+        }
+        
+        // if a key is released, we put the sprite into its last stable frame
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            spriteRenderer.sprite = spriteArray[4];
+            spriteFrame = 0;
+        }
+        else if (Input.GetKeyUp(KeyCode.A))
+        {
+            spriteRenderer.sprite = spriteArray[12];
+            spriteFrame = 0;
+        }
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            spriteRenderer.sprite = spriteArray[0];
+            spriteFrame = 0;
+
+        }
+        else if (Input.GetKeyUp(KeyCode.D))
+        {
+            spriteRenderer.sprite = spriteArray[8];
+            spriteFrame = 0;
+        }
+    }
+
+    // these functions work by checking if a certain amount of time has elapsed and if so, it goes to the next frame and cycles
+    void playLeftAnimation()
+    {
+        timer += Time.deltaTime;
+        if (timer > animationSpeed)
+        {
+            spriteRenderer.sprite = spriteArray[(spriteFrame % 4) + 12];
+            timer -= animationSpeed;
+            spriteFrame = ((spriteFrame + 1) % 4) + 12;
+        }
+    }
+
+    void playRightAnimation()
+    {
+        timer += Time.deltaTime;
+        if (timer > animationSpeed)
+        {
+            spriteRenderer.sprite = spriteArray[(spriteFrame % 4) + 8];
+            timer -= animationSpeed;
+            spriteFrame = ((spriteFrame + 1) % 4) + 8;
+        }
+    }
+
+    void playUpAnimation()
+    {
+        timer += Time.deltaTime;
+        if (timer > animationSpeed)
+        {
+            spriteRenderer.sprite = spriteArray[(spriteFrame % 4) + 4];
+            timer -= animationSpeed;
+            spriteFrame = ((spriteFrame + 1) % 4) + 4;
+        }
+    }
+
+    void playDownAnimation()
+    {
+        timer += Time.deltaTime;
+        if (timer > animationSpeed)
+        {
+            spriteRenderer.sprite = spriteArray[(spriteFrame % 4) + 0];
+            timer -= animationSpeed;
+            spriteFrame = ((spriteFrame + 1) % 4) + 0;
+        }
     }
 }
