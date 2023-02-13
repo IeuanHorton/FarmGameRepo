faddc8913cd61b0d8b38f931df158e5079265b8d
Ieuan <Hortonieuan@gmail.com>
2023-02-12 19:38:09 -0500

    Added a Item and Crop Script
    Also added a a few test crops

---
+++ FarmGame/Assets/Code/Items/Crop.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Crop")]
+public class Crop : Item
+{
+    [SerializeField]
+    private CropType type;
+
+    
+}
+
+public enum CropType {Fruit, Vegetable}
---
+++ FarmGame/Assets/Code/Items/Item.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Inventory/Item")]
+public class Item : ScriptableObject
+{
+    [SerializeField]
+    new private string name = "New Name";
+    [SerializeField]
+    private Sprite icon = null;
+    [SerializeField]
+    private int worth = 0;
+
+    //When the player double left clicks on the object
+    public virtual void Use()
+    {
+
+    }
+
+    public void changeWorth(int x)
+    {
+        worth = x;
+    }
+}
